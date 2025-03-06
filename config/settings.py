d9c360a45f681b3b6649e4bf86effc60351e3c9a
Muhammadbilol222 <shaxlomirboboyeva@gmail.com>
2025-03-06 07:47:58 +0500

    heroku uchun ozgarish

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h+45g$och-sc2m1i+@op1pw1dwcht^csck&c_vs_$zxi#ed_7-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
